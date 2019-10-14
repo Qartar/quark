a21b3049b0c7352f899d35736ec82b74e28d117c
Liz Nguyen <liz@liznguyen.net>
2019-10-13 20:14:26 -0700

    Fixed OSX socket definitions and SDL paths

--- render/sdl_window.cpp
+++ render/sdl_window.cpp
@@ -42,7 +42,7 @@ result window::create(int xpos, int ypos, int width, int height, bool fullscreen
 {
     int style = SDL_WINDOW_OPENGL
         | SDL_WINDOW_HIDDEN
-        | SDL_WINDOW_ALLOW_HIGHDPI
+        //| SDL_WINDOW_ALLOW_HIGHDPI
         | (fullscreen ? SDL_WINDOW_FULLSCREEN : 0);
 
     _hwnd = SDL_CreateWindow("Quark", xpos, ypos, width, height, style);
