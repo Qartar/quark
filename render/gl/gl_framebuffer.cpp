4452d67339185751550d5705fc8d7154c1b3b206
Carter Bray <qartar@live.com>
2020-06-22 16:49:00 -0700

    Add support for direct state access functions, remove renderbuffer functions

--- render/gl/gl_framebuffer.cpp
+++ render/gl/gl_framebuffer.cpp
@@ -11,34 +11,40 @@
 namespace render {
 namespace gl {
 
-framebuffer::PFNGLBINDRENDERBUFFER framebuffer::glBindRenderbuffer = nullptr;
-framebuffer::PFNGLDELETERENDERBUFFERS framebuffer::glDeleteRenderbuffers = nullptr;
-framebuffer::PFNGLGENRENDERBUFFERS framebuffer::glGenRenderbuffers = nullptr;
-framebuffer::PFNGLRENDERBUFFERSTORAGE framebuffer::glRenderbufferStorage = nullptr;
-framebuffer::PFNGLRENDERBUFFERSTORAGEMULTISAMPLE framebuffer::glRenderbufferStorageMultisample = nullptr;
+// GL_ARB_framebuffer_object
 framebuffer::PFNGLBINDFRAMEBUFFER framebuffer::glBindFramebuffer = nullptr;
 framebuffer::PFNGLDELETEFRAMEBUFFERS framebuffer::glDeleteFramebuffers = nullptr;
 framebuffer::PFNGLGENFRAMEBUFFERS framebuffer::glGenFramebuffers = nullptr;
-framebuffer::PFNGLFRAMEBUFFERRENDERBUFFER framebuffer::glFramebufferRenderbuffer = nullptr;
 framebuffer::PFNGLFRAMEBUFFERTEXTURE2D framebuffer::glFramebufferTexture2D = nullptr;
 framebuffer::PFNGLBLITFRAMEBUFFER framebuffer::glBlitFramebuffer = nullptr;
+
+// GL_ARB_direct_state_access
+framebuffer::PFNGLCREATEFRAMEBUFFERS framebuffer::glCreateFramebuffers = nullptr;
+framebuffer::PFNGLNAMEDFRAMEBUFFERTEXTURE framebuffer::glNamedFramebufferTexture = nullptr;
+framebuffer::PFNGLNAMEDFRAMEBUFFERDRAWBUFFER framebuffer::glNamedFramebufferDrawBuffer = nullptr;
+framebuffer::PFNGLNAMEDFRAMEBUFFERDRAWBUFFERS framebuffer::glNamedFramebufferDrawBuffers = nullptr;
+framebuffer::PFNGLNAMEDFRAMEBUFFERREADBUFFER framebuffer::glNamedFramebufferReadBuffer = nullptr;
+framebuffer::PFNGLBLITNAMEDFRAMEBUFFER framebuffer::glBlitNamedFramebuffer = nullptr;
 
 framebuffer::PFNGLDRAWBUFFERS framebuffer::glDrawBuffers = nullptr;
 
 //------------------------------------------------------------------------------
 void framebuffer::init()
 {
-    glBindRenderbuffer = (PFNGLBINDRENDERBUFFER )wglGetProcAddress("glBindRenderbuffer");
-    glDeleteRenderbuffers = (PFNGLDELETERENDERBUFFERS )wglGetProcAddress("glDeleteRenderbuffers");
-    glGenRenderbuffers = (PFNGLGENRENDERBUFFERS )wglGetProcAddress("glGenRenderbuffers");
-    glRenderbufferStorage = (PFNGLRENDERBUFFERSTORAGE )wglGetProcAddress("glRenderbufferStorage");
-    glRenderbufferStorageMultisample = (PFNGLRENDERBUFFERSTORAGEMULTISAMPLE )wglGetProcAddress("glRenderbufferStorageMultisample");
+    // GL_ARB_framebuffer_object
     glBindFramebuffer = (PFNGLBINDFRAMEBUFFER )wglGetProcAddress("glBindFramebuffer");
     glDeleteFramebuffers = (PFNGLDELETEFRAMEBUFFERS )wglGetProcAddress("glDeleteFramebuffers");
     glGenFramebuffers = (PFNGLGENFRAMEBUFFERS )wglGetProcAddress("glGenFramebuffers");
-    glFramebufferRenderbuffer = (PFNGLFRAMEBUFFERRENDERBUFFER )wglGetProcAddress("glFramebufferRenderbuffer");
     glFramebufferTexture2D = (PFNGLFRAMEBUFFERTEXTURE2D )wglGetProcAddress("glFramebufferTexture2D");
     glBlitFramebuffer = (PFNGLBLITFRAMEBUFFER )wglGetProcAddress("glBlitFramebuffer");
+
+    // GL_ARB_direct_state_access
+    glCreateFramebuffers = (PFNGLCREATEFRAMEBUFFERS )wglGetProcAddress("glCreateFramebuffers");
+    glNamedFramebufferTexture = (PFNGLNAMEDFRAMEBUFFERTEXTURE )wglGetProcAddress("glNamedFramebufferTexture");
+    glNamedFramebufferDrawBuffer = (PFNGLNAMEDFRAMEBUFFERDRAWBUFFER )wglGetProcAddress("glNamedFramebufferDrawBuffer");
+    glNamedFramebufferDrawBuffers = (PFNGLNAMEDFRAMEBUFFERDRAWBUFFERS )wglGetProcAddress("glNamedFramebufferDrawBuffers");
+    glNamedFramebufferReadBuffer = (PFNGLNAMEDFRAMEBUFFERREADBUFFER )wglGetProcAddress("glNamedFramebufferReadBuffer");
+    glBlitNamedFramebuffer = (PFNGLBLITNAMEDFRAMEBUFFER )wglGetProcAddress("glBlitNamedFramebuffer");
 
     glDrawBuffers = (PFNGLDRAWBUFFERS )wglGetProcAddress("glDrawBuffers");
 }
@@ -84,8 +90,12 @@ framebuffer::framebuffer(GLsizei width, GLsizei height, attachment const* attach
     , _width(width)
     , _height(height)
 {
-    glGenFramebuffers(1, &_name);
-    glBindFramebuffer(GL_FRAMEBUFFER, _name);
+    if (glCreateFramebuffers) {
+        glCreateFramebuffers(1, &_name);
+    } else if (glGenFramebuffers) {
+        glGenFramebuffers(1, &_name);
+        glBindFramebuffer(GL_FRAMEBUFFER, _name);
+    }
 
     std::size_t num_color_attachments = 0;
     std::size_t num_depth_attachments = 0;
@@ -96,8 +106,7 @@ framebuffer::framebuffer(GLsizei width, GLsizei height, attachment const* attach
         switch (attachments[ii]._type) {
             case attachment_type::color:
                 _color_attachments.push_back(texture2d(1, attachments[ii]._format, _width, _height));
-                glFramebufferTexture2D(
-                    GL_FRAMEBUFFER,
+                attach(
                     narrow_cast<GLenum>(GL_COLOR_ATTACHMENT0 + num_color_attachments),
                     GL_TEXTURE_2D,
                     _color_attachments[num_color_attachments].name(),
@@ -110,8 +119,7 @@ framebuffer::framebuffer(GLsizei width, GLsizei height, attachment const* attach
                     continue;
                 }
                 _depth_attachment = texture2d(1, attachments[ii]._format, _width, _height);
-                glFramebufferTexture2D(
-                    GL_FRAMEBUFFER,
+                attach(
                     GL_DEPTH_ATTACHMENT,
                     GL_TEXTURE_2D,
                     _depth_attachment.name(),
@@ -124,8 +132,7 @@ framebuffer::framebuffer(GLsizei width, GLsizei height, attachment const* attach
                     continue;
                 }
                 _stencil_attachment = texture2d(1, attachments[ii]._format, _width, _height);
-                glFramebufferTexture2D(
-                    GL_FRAMEBUFFER,
+                attach(
                     GL_STENCIL_ATTACHMENT,
                     GL_TEXTURE_2D,
                     _stencil_attachment.name(),
@@ -138,8 +145,7 @@ framebuffer::framebuffer(GLsizei width, GLsizei height, attachment const* attach
                     continue;
                 }
                 _depth_stencil_attachment = texture2d(1, attachments[ii]._format, _width, _height);
-                glFramebufferTexture2D(
-                    GL_FRAMEBUFFER,
+                attach(
                     GL_DEPTH_STENCIL_ATTACHMENT,
                     GL_TEXTURE_2D,
                     _depth_stencil_attachment.name(),
@@ -156,8 +162,12 @@ framebuffer::framebuffer(GLsizei samples, GLsizei width, GLsizei height, attachm
     , _width(width)
     , _height(height)
 {
-    glGenFramebuffers(1, &_name);
-    glBindFramebuffer(GL_FRAMEBUFFER, _name);
+    if (glCreateFramebuffers) {
+        glCreateFramebuffers(1, &_name);
+    } else if (glGenFramebuffers) {
+        glGenFramebuffers(1, &_name);
+        glBindFramebuffer(GL_FRAMEBUFFER, _name);
+    }
 
     std::size_t num_color_attachments = 0;
     std::size_t num_depth_attachments = 0;
@@ -168,8 +178,7 @@ framebuffer::framebuffer(GLsizei samples, GLsizei width, GLsizei height, attachm
         switch (attachments[ii]._type) {
             case attachment_type::color:
                 _color_attachments.push_back(texture2dmultisample(samples, attachments[ii]._format, _width, _height, GL_FALSE));
-                glFramebufferTexture2D(
-                    GL_FRAMEBUFFER,
+                attach(
                     narrow_cast<GLenum>(GL_COLOR_ATTACHMENT0 + num_color_attachments),
                     GL_TEXTURE_2D_MULTISAMPLE,
                     _color_attachments[num_color_attachments].name(),
@@ -182,8 +191,7 @@ framebuffer::framebuffer(GLsizei samples, GLsizei width, GLsizei height, attachm
                     continue;
                 }
                 _depth_attachment = texture2dmultisample(samples, attachments[ii]._format, _width, _height, GL_FALSE);
-                glFramebufferTexture2D(
-                    GL_FRAMEBUFFER,
+                attach(
                     GL_DEPTH_ATTACHMENT,
                     GL_TEXTURE_2D_MULTISAMPLE,
                     _depth_attachment.name(),
@@ -196,8 +204,7 @@ framebuffer::framebuffer(GLsizei samples, GLsizei width, GLsizei height, attachm
                     continue;
                 }
                 _stencil_attachment = texture2dmultisample(samples, attachments[ii]._format, _width, _height, GL_FALSE);
-                glFramebufferTexture2D(
-                    GL_FRAMEBUFFER,
+                attach(
                     GL_STENCIL_ATTACHMENT,
                     GL_TEXTURE_2D_MULTISAMPLE,
                     _stencil_attachment.name(),
@@ -210,8 +217,7 @@ framebuffer::framebuffer(GLsizei samples, GLsizei width, GLsizei height, attachm
                     continue;
                 }
                 _depth_stencil_attachment = texture2dmultisample(samples, attachments[ii]._format, _width, _height, GL_FALSE);
-                glFramebufferTexture2D(
-                    GL_FRAMEBUFFER,
+                attach(
                     GL_DEPTH_STENCIL_ATTACHMENT,
                     GL_TEXTURE_2D_MULTISAMPLE,
                     _depth_stencil_attachment.name(),
@@ -219,6 +225,16 @@ framebuffer::framebuffer(GLsizei samples, GLsizei width, GLsizei height, attachm
                 ++num_depth_stencil_attachments;
                 break;
         }
+    }
+}
+
+//------------------------------------------------------------------------------
+void framebuffer::attach(GLenum attachment, GLenum target, GLuint texture, GLuint level)
+{
+    if (glNamedFramebufferTexture) {
+        glNamedFramebufferTexture(_name, attachment, texture, level);
+    } else if (glFramebufferTexture2D) {
+        glFramebufferTexture2D(GL_FRAMEBUFFER, attachment, target, texture, level);
     }
 }
 
@@ -243,22 +259,34 @@ void framebuffer::draw() const
 //------------------------------------------------------------------------------
 void framebuffer::read_buffer(GLenum buf) const
 {
-    glBindFramebuffer(GL_READ_FRAMEBUFFER, _name);
-    glReadBuffer(buf);
+    if (glNamedFramebufferReadBuffer) {
+        glNamedFramebufferReadBuffer(_name, buf);
+    } else if (glBindFramebuffer) {
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, _name);
+        glReadBuffer(buf);
+    }
 }
 
 //------------------------------------------------------------------------------
 void framebuffer::draw_buffer(GLenum buf) const
 {
-    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, _name);
-    glDrawBuffer(buf);
+    if (glNamedFramebufferDrawBuffer) {
+        glNamedFramebufferDrawBuffer(_name, buf);
+    } else if (glBindFramebuffer) {
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, _name);
+        glDrawBuffer(buf);
+    }
 }
 
 //------------------------------------------------------------------------------
 void framebuffer::draw_buffers(GLsizei n, GLenum const* bufs) const
 {
-    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, _name);
-    glDrawBuffers(n, bufs);
+    if (glNamedFramebufferDrawBuffers) {
+        glNamedFramebufferDrawBuffers(_name, n, bufs);
+    } else if (glBindFramebuffer) {
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, _name);
+        glDrawBuffers(n, bufs);
+    }
 }
 
 //------------------------------------------------------------------------------
@@ -270,9 +298,13 @@ void framebuffer::blit(framebuffer const& src, GLbitfield mask, GLenum filter) c
 //------------------------------------------------------------------------------
 void framebuffer::blit(framebuffer const& src, GLint srcX0, GLint srcY0, GLint srcX1, GLint srcY1, GLint dstX0, GLint dstY0, GLint dstX1, GLint dstY1, GLbitfield mask, GLenum filter) const
 {
-    src.read();
-    draw();
-    glBlitFramebuffer(srcX0, srcY0, srcX1, srcY1, dstX0, dstY0, dstX1, dstY1, mask, filter);
+    if (glBlitNamedFramebuffer) {
+        glBlitNamedFramebuffer(src._name, _name, srcX0, srcY0, srcX1, srcY1, dstX0, dstY0, dstX1, dstY1, mask, filter);
+    } else if (glBlitFramebuffer) {
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, src._name);
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, _name);
+        glBlitFramebuffer(srcX0, srcY0, srcX1, srcY1, dstX0, dstY0, dstX1, dstY1, mask, filter);
+    }
 }
 
 } // namespace gl
