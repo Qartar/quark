4294e62e5445989c2a9dadfd377d88658126d004
Carter Bray <qartar@live.com>
2023-08-03 14:49:51 -0700

    Add ineffecient function for calculating curve arclength

--- editor/ed_ship_editor.h
+++ editor/ed_ship_editor.h
@@ -72,6 +72,24 @@ protected:
             vec2 u = evaluate_tangent(t);
             return cross(u, uu) / (length(u) * length_sqr(u));
         }
+
+        float evaluate_arclength(float t) const {
+            vec2 p1 = evaluate(t);
+            return evaluate_arclength_r(0.f, t, a, p1, length(p1 - a));
+        }
+
+        float evaluate_arclength_r(float t0, float t1, vec2 p0, vec2 p1, float s0) const {
+            float tm = .5f * (t0 + t1);
+            vec2 pm = evaluate(tm);
+            float s1 = length(pm - p0);
+            float s2 = length(p1 - pm);
+            if ((s1 + s2) <= s0 * 1.001f) {
+                return s1 + s2;
+            } else {
+                return evaluate_arclength_r(t0, tm, p0, pm, s1)
+                     + evaluate_arclength_r(tm, t1, pm, p1, s2);
+            }
+        }
     };
 
     struct ccurve {
