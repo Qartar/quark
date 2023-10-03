3a7f9b1419cfc8f4be8a8142e28e65f4285f7f1e
Carter Bray <qartar@live.com>
2023-10-03 12:34:27 -0700

    Move application object off of the stack

--- system/win_main.cpp
+++ system/win_main.cpp
@@ -52,7 +52,8 @@ application* application::_singleton = nullptr;
 //------------------------------------------------------------------------------
 int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR szCmdLine, int nCmdShow)
 {
-    return application(hInstance).main(szCmdLine, nCmdShow);
+    static application app(hInstance);
+    return app.main(szCmdLine, nCmdShow);
 }
 
 //------------------------------------------------------------------------------
