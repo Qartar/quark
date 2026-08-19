7752b69cb6f0179f8f60e959d3c5265e5d194bfd
Carter Bray <qartar@live.com>
2026-08-19 16:32:12 -0700

    Add lexer for parsing text-based assets
    Preparation for moving design files into text-based asset files. The
    lexer is very rudimentary and only supports operations necessary for
    parsing design files currently.

---
+++ shared/cm_lexer.cpp
@@ -0,0 +1,468 @@
+// cm_lexer.cpp
+//
+
+#include "cm_lexer.h"
+#include "cm_filesystem.h"
+
+#include <cstdarg>
+#include <cstdio>
+
+////////////////////////////////////////////////////////////////////////////////
+namespace {
+string::literal token_type_string(lexer::token_type type)
+{
+    switch (type) {
+        case lexer::token_type::name: return "name";
+        case lexer::token_type::string: return "string";
+        case lexer::token_type::number: return "number";
+        case lexer::token_type::punctuation: return "punctuation";
+    }
+    __assume(false);
+}
+} // anonymous namespace
+
+//------------------------------------------------------------------------------
+lexer::lexer(string::view filename)
+    : _filename(filename)
+    , _linenumber(1)
+    , _last_error{}
+    , _next_token(0)
+{
+    file::stream s = file::open(filename, file::mode::read);
+    _buffer.resize(s.size());
+    s.read((file::byte*)_buffer.data(), s.size());
+    _buffer[_buffer.length()] = 0;
+    s.close();
+
+    split_lines();
+
+    tokenize();
+}
+
+//------------------------------------------------------------------------------
+bool lexer::has_token() const {
+    return _next_token < _tokens.size();
+}
+
+//------------------------------------------------------------------------------
+bool lexer::peek_token(string::view expected) const
+{
+    if (_next_token < _tokens.size() && _tokens[_next_token] == expected) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//------------------------------------------------------------------------------
+bool lexer::peek_token_type(token_type expected_type) const
+{
+    if (_next_token < _tokens.size() && _tokens[_next_token].type == expected_type) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//------------------------------------------------------------------------------
+bool lexer::check_token(string::view expected)
+{
+    if (_next_token < _tokens.size() && _tokens[_next_token] == expected) {
+        ++_next_token;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//------------------------------------------------------------------------------
+bool lexer::expect_token(string::view expected)
+{
+    if (_next_token >= _tokens.size()) {
+        set_error({_buffer.end(), _buffer.end(), _buffer.end()}, "expected token");
+        return false;
+    } else if (_tokens[_next_token] != expected) {
+        set_error(_tokens[_next_token], "expected token '%.*s', found '%.*s'",
+            int(expected.length()), expected.begin(),
+            int(_tokens[_next_token].end - _tokens[_next_token].begin),
+            _tokens[_next_token].begin);
+        return false;
+    } else {
+        ++_next_token;
+        return true;
+    }
+}
+
+//------------------------------------------------------------------------------
+bool lexer::expect_any_token(lexer::token& t)
+{
+    if (_next_token >= _tokens.size()) {
+        set_error({_buffer.end(), _buffer.end(), _buffer.end()}, "expected token");
+        return false;
+    } else {
+        t = _tokens[_next_token++];
+        return true;
+    }
+}
+
+//------------------------------------------------------------------------------
+bool lexer::expect_token_type(lexer::token& t, token_type type)
+{
+    if (_next_token >= _tokens.size()) {
+        set_error({_buffer.end(), _buffer.end(), _buffer.end()}, "expected token");
+        return false;
+    } else if (_tokens[_next_token].type != type) {
+        set_error(_tokens[_next_token], "expected %s, found '%.*s'",
+            token_type_string(type).c_str(),
+            int(_tokens[_next_token].end - _tokens[_next_token].begin),
+            _tokens[_next_token].begin);
+        return false;
+    } else {
+        t = _tokens[_next_token++];
+        return true;
+    }
+}
+
+//------------------------------------------------------------------------------
+bool lexer::parse(string::buffer& s)
+{
+    token t;
+    if (expect_token_type(t, token_type::string)) {
+        assert(*t.begin == '\"');
+        assert(*(t.end - 1) == '\"');
+
+        std::size_t len = 0;
+        for (char const* ptr = t.begin + 1; ptr < t.end - 1; ++ptr) {
+            if (*ptr == '\\') {
+                switch (*++ptr) {
+                    case '\'':
+                    case '\"':
+                    case '\\':
+                        break;
+                    default:
+                        // invalid or unsupported escape sequence, copy as-is
+                        assert(false); // should have caught this during tokenization
+                        ++len;
+                        break;
+                }
+            }
+            ++len;
+        }
+
+        s.resize(len);
+        char* out = s.data();
+        for (char const* ptr = t.begin + 1; ptr < t.end - 1; ++ptr) {
+            if (*ptr == '\\') {
+                switch (*++ptr) {
+                    case '\'':
+                    case '\"':
+                    case '\\':
+                        *out++ = *ptr;
+                        break;
+                    default:
+                        // invalid or unsupported escape sequence, copy as-is
+                        *out++ = '\\';
+                        *out++ = *ptr;
+                        break;
+                }
+            } else {
+                *out++ = *ptr;
+            }
+        }
+        *out = '\0';
+        return true;
+    } else  {
+        return false;
+    }
+}
+
+//------------------------------------------------------------------------------
+bool lexer::parse(int& i)
+{
+    token t;
+    if (expect_token_type(t, token_type::number)) {
+        return (_snscanf_s(t.begin, t.end - t.begin, "%d", &i) == 1);
+    } else  {
+        return false;
+    }
+}
+
+//------------------------------------------------------------------------------
+bool lexer::parse(float& f)
+{
+    token t;
+    if (expect_token_type(t, token_type::number)) {
+        return (_snscanf_s(t.begin, t.end - t.begin, "%f", &f) == 1);
+    } else  {
+        return false;
+    }
+}
+
+//------------------------------------------------------------------------------
+bool lexer::parse(double& d)
+{
+    token t;
+    if (expect_token_type(t, token_type::number)) {
+        return (_snscanf_s(t.begin, t.end - t.begin, "%lf", &d) == 1);
+    } else  {
+        return false;
+    }
+}
+
+//------------------------------------------------------------------------------
+bool lexer::parse(vec2& v)
+{
+    if (expect_token("(")
+        && parse(v.x)
+        && expect_token(",")
+        && parse(v.y)
+        && expect_token(")")) {
+        return true;
+    } else  {
+        return false;
+    }
+}
+
+//------------------------------------------------------------------------------
+bool lexer::parse(vec3& v)
+{
+    if (expect_token("(")
+        && parse(v.x)
+        && expect_token(",")
+        && parse(v.y)
+        && expect_token(",")
+        && parse(v.z)
+        && expect_token(")")) {
+        return true;
+    } else  {
+        return false;
+    }
+}
+
+//------------------------------------------------------------------------------
+bool lexer::locate(token const& t, location& l) const
+{
+    if (t.begin < _buffer.begin() || t.begin > _buffer.end()) {
+        return false;
+    }
+
+    auto it = std::upper_bound(_lines.begin(), _lines.end(), t.begin,
+        [](char const* begin, char const* rhs) {
+            return begin < rhs;
+        }
+    );
+
+    assert(it != _lines.begin());
+    --it;
+
+    l = {
+        _filename,
+        _linenumber + std::distance(_lines.begin(), it),
+        1 + static_cast<std::size_t>(t.begin - *it),
+    };
+    return true;
+}
+
+//------------------------------------------------------------------------------
+void lexer::set_error(token t, string::literal fmt, ...)
+{
+    // Try to locate token line/column within the source
+    if (!locate(t, _last_error)) {
+        _last_error.filename = _filename;
+        _last_error.line = 0;
+        _last_error.column = 0;
+    }
+
+    // Concatenate filename, line/column if available, and error message
+    int location_len = _last_error.column ? _scprintf("(%zu:%zu): ", _last_error.line, _last_error.column)
+        : _last_error.line ? _scprintf("(%zu): ", _last_error.line) : 2;
+
+    va_list ap;
+
+    va_start(ap, fmt);
+    int message_len = _vscprintf(fmt.c_str(), ap);
+    va_end(ap);
+
+    _last_error.message.resize(_filename.length() + location_len + message_len);
+    strncpy_s(_last_error.message.data(), _last_error.message.length() + 1, _filename.begin(), _filename.length());
+
+    if (_last_error.column) {
+        _snprintf_s(
+            _last_error.message.data() + _filename.length(),
+            _last_error.message.length() + 1 - _filename.length(),
+            location_len,
+            "(%zu:%zu): ", _last_error.line, _last_error.column);
+    } else if (_last_error.line) {
+        _snprintf_s(
+            _last_error.message.data() + _filename.length(),
+            _last_error.message.length() + 1 - _filename.length(),
+            location_len,
+            "(%zu): ", _last_error.line);
+    } else {
+        strncpy_s(
+            _last_error.message.data() + _filename.length(),
+            _last_error.message.length() + 1 - _filename.length(),
+            ": ",
+            2);
+    }
+
+    va_start(ap, fmt);
+    _vsnprintf_s(
+        _last_error.message.data() + _filename.length() + location_len,
+        _last_error.message.length() + 1 - _filename.length() - location_len,
+        message_len,
+        fmt.c_str(), ap);
+    va_end(ap);
+}
+
+//------------------------------------------------------------------------------
+void lexer::split_lines()
+{
+    assert(_buffer.begin());
+
+    char const* begin = _buffer.begin();
+    char const* end = begin;
+
+    while (end = strstr(end, "\n")) {
+        _lines.push_back(begin);
+        begin = ++end;
+    }
+
+    if (begin != end) {
+        _lines.push_back(begin);
+    }
+}
+
+//------------------------------------------------------------------------------
+bool lexer::tokenize()
+{
+    assert(_buffer.begin());
+    assert(_buffer.end());
+
+    char const* str = _buffer.begin();
+    char const* end = _buffer.end();
+
+    while (true) {
+        char const* whitespace = str;
+
+        // skip leading whitespace
+        while (str < end && *str <= ' ') {
+            assert(*str);
+            ++str;
+        }
+
+        if (str >= end) {
+            return true;
+        }
+
+        // skip comments
+        if (str + 1 < end && str[0] == '/') {
+            if (str[1] == '/') {
+                while (str < end && *str != '\n') {
+                    ++str;
+                }
+                continue;
+            } else if (str[1] == '*') {
+                char const* ptr = str;
+                str += 2;
+                while (true) {
+                    if (str + 1 >= end) {
+                        set_error({whitespace, ptr, ptr + 2}, "unexpected end of file in comment");
+                        return false;
+                    } else if (str[0] == '*' && str[1] == '/') {
+                        str += 2;
+                        break;
+                    } else {
+                        ++str;
+                    }
+                }
+                continue;
+            }
+        }
+
+        // evaluate number token
+        char const* begin = str;
+        if ((*str >= '0' && *str <= '9') || *str == '.'
+            || (*str == '-' && str + 1 < end && ((str[1] >= '0' && str[1] <= '9') || str[1] =='.'))) {
+            bool has_dot = false;
+            if (*str == '-') {
+                ++str;
+            }
+            while (str < end && (*str >= '0' && *str <= '9' || (!has_dot && *str == '.'))) {
+                if (*str == '.') {
+                    if (has_dot) {
+                        set_error({whitespace, begin, str}, "invalid literal");
+                        return false;
+                    }
+                    has_dot = true;
+                }
+                ++str;
+            }
+            _tokens.push_back({whitespace, begin, str, token_type::number});
+            continue;
+        }
+
+        // evaluate punctuation token
+        switch (*str) {
+            case '=':
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '^':
+            case '(':
+            case ')':
+            case '{':
+            case '}':
+            case '[':
+            case ']':
+            case ':':
+            case ';':
+            case ',':
+            case '|':
+                ++str;
+                _tokens.push_back({whitespace, begin, str, token_type::punctuation});
+                continue;
+        }
+
+        // evaluate name token
+        if (*str >= 'a' && *str <= 'z' || *str >= 'A' && *str <= 'Z') {
+            while (str < end && (*str == '_' || *str >= '0' && *str <= '9' || *str >= 'a' && *str <= 'z' || *str >= 'A' && *str <= 'Z')) {
+                ++str;
+            }
+            _tokens.push_back({whitespace, begin, str, token_type::name});
+            continue;
+        }
+
+        // evaluate string token
+        if (*str == '\"') {
+            while (true) {
+                if (++str >= end) {
+                    set_error({whitespace, begin, str}, "unexpected end of file in string literal");
+                    return false;
+                } else if (*str == '\\') {
+                    switch (*++str) {
+                        // Only a small set of escape sequences are allowed, cf. parse(string::buffer&)
+                        case '\'':
+                        case '\"':
+                        case '\\':
+                            break;
+                        default:
+                            set_error({whitespace, begin, str}, "invalid escape sequence in string literal");
+                            return false;
+                    }
+                } else if (*str == '\"') {
+                    ++str;
+                    break;
+                }
+            }
+            _tokens.push_back({whitespace, begin, str, token_type::string});
+            continue;
+        }
+
+        set_error({str, str, str+1}, "invalid character '%c'", *str);
+        return false;
+    }
+
+    // never gets here
+}
---
+++ shared/cm_lexer.h
@@ -0,0 +1,123 @@
+// cm_lexer.h
+//
+
+#pragma once
+
+#include "cm_string.h"
+#include "cm_vector.h"
+#include <vector>
+
+////////////////////////////////////////////////////////////////////////////////
+class lexer
+{
+public:
+    struct location {
+        string::view filename;
+        std::size_t line;
+        std::size_t column;
+    };
+
+    struct error : location {
+        string::buffer message;
+    };
+
+    enum class token_type {
+        name, //!< Alpha-numeric identifier
+        string, //!< String literal delimited by double quotes, e.g. "Hello"
+        number, //!< Numeric token, either integer or decimal
+        punctuation, //!< Other special characters
+    };
+
+    struct token {
+        char const* whitespace; //!< Beginning of the token including leading whitespace
+        char const* begin; //!< Beginning of the token, skipping whitespace
+        char const* end; //!< End of the token
+        token_type type;
+
+        //! Implicit conversion to string::view
+        operator string::view() const {
+            return string::view(begin, end);
+        }
+    };
+
+public:
+    lexer(string::view filename);
+
+    //! Returns true if there have been any errors.
+    bool has_error() const { return _last_error.message.length() > 0; }
+    //! Returns the most recent error.
+    error last_error() const { return _last_error; }
+
+    //! Populates location data for the given token if it can be located and returns true.
+    bool locate(token const& t, location& l) const;
+
+    //! Returns true if there are any remaining tokens.
+    bool has_token() const;
+
+    //! Returns true if the next token matches the given string, does not advance the cursor.
+    bool peek_token(string::view expected) const;
+
+    //! Returns true if the next token type matches the given type, does not advance the cursor.
+    bool peek_token_type(token_type expected_type) const;
+
+    //! Returns true and advances the cursor if the next token matches the given string.
+    bool check_token(string::view expected);
+
+    //! Returns true and advances the cursor if the next token matches the given
+    //! string. If there are no remaining tokens or the next token does not match
+    //! the given string an error is generated.
+    bool expect_token(string::view expected);
+
+    //! Returns true, populates the given token, and advances the cursor if there
+    //! are any remaining tokens.
+    bool expect_any_token(token& t);
+
+    //! Returns true, populates the given token, and advances the cursor if the
+    //! next token has the given token type.
+    bool expect_token_type(token& t, token_type type);
+
+    //! Parse the next token as a string. Token must be type `string`.
+    bool parse(string::buffer& s);
+
+    //! Parse the next token as an integer. Token must be type `number`.
+    bool parse(int& i);
+
+    //! Parse the next token as a single-precision float. Token must be type `number`.
+    bool parse(float& f);
+
+    //! Parse the next token as a double-precision float. Token must be type `number`.
+    bool parse(double& d);
+
+    //! Parse the next five tokens as a vec2. Tokens must be `(` `number` `,` `number` `)`
+    bool parse(vec2& v);
+
+    //! Parse the next seven tokens as a vec3. Tokens must be `(` `number` `,` `number` `,` `number` `)`
+    bool parse(vec3& v);
+
+protected:
+    //! Source filename
+    string::buffer _filename;
+    //! Starting line number (typically 1)
+    std::size_t _linenumber;
+
+    //! String buffer for tokens to point into
+    string::buffer _buffer;
+    //! Pointer into buffer at the start of each line
+    std::vector<char const*> _lines;
+    //! All tokens, points into _buffer
+    std::vector<token> _tokens;
+
+    //! Most recent error
+    error _last_error;
+    //! Next token to be parsed/consumed
+    std::size_t _next_token;
+
+protected:
+    void set_error(token t, string::literal fmt, ...);
+
+    //! Splits the source text into lines, used for locating token line/column
+    void split_lines();
+
+    //! Splits the source text into discrete tokens
+    bool tokenize();
+};
